b997fb29405563895486379360a024c80d6a2cd9
Sergio León <sergioleon4004@hotmail.com>
2023-02-22 10:35:10 -0500

    style: spaces have been erased

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ use LionRoute\Route;
  * ------------------------------------------------------------------------------
  **/
 
-Route::prefix("companies", function(){
+Route::prefix("companies", function() {
     Route::post('create', [CompaniesController::class, 'createCompanies']);
     Route::post('update', [CompaniesController::class, 'updateCompanies']);
     Route::get('readCompaniesSelector', [CompaniesController::class, 'readCompaniesSelector']);
 
-    Route::prefix("requirements", function(){
+    Route::prefix("requirements", function() {
         Route::post('create', [RequirementsController::class, 'createRequirements']);
         Route::post('update', [RequirementsController::class, 'updateRequirements']);
         Route::post('requirementsByclients', [RequirementsController::class, 'readRequirementsByClients']);
@@ -33,30 +33,30 @@ Route::prefix("companies", function(){
     });
 });
 
-Route::prefix("assignment", function(){
+Route::prefix("assignment", function() {
     Route::post('create', [AssignmentRequirementsController::class, 'createAssignmentrequirements']);
     Route::post('update', [AssignmentRequirementsController::class, 'update_assignment_requirements']);
 
-    Route::prefix("read", function(){
+    Route::prefix("read", function() {
         Route::get('select', [AssignmentRequirementsController::class, 'ReadAssignmentRequirementsSelect']);
     });
 
-    Route::prefix("developers", function(){
+    Route::prefix("developers", function() {
         Route::post('create', [AssignmentRequirementsHasDevelopersController::class, 'createAssignmentRequirementsHasDevelopers']);
         Route::post('delete', [AssignmentRequirementsHasDevelopersController::class, 'deletesignmentRequirementsHasDevelopers']);
         Route::post('update', [AssignmentRequirementsHasDevelopersController::class, 'updatessignmentRequirementsHasDevelopers']);
 
-        Route::prefix("read", function(){
+        Route::prefix("read", function() {
             Route::get('assigment', [AssignmentRequirementsHasDevelopersController::class, 'readAssigmentHasDevelopers']);
         });
     });
 });
 
-Route::prefix("developers", function(){
+Route::prefix("developers", function() {
     Route::post('create', [DevelopersController::class, 'createDevelopers']);
     Route::post('update', [DevelopersController::class, 'updateDevelopers']);
 
-    Route::prefix("read", function(){
+    Route::prefix("read", function() {
         Route::get('select', [DevelopersController::class, 'readDevelopersSelect']);
     });
 });
